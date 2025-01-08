283bf2690d80b5ecb877894e0fafba3b6afbc3c3
faileon <pt4cekt@gmail.com>
2025-01-08 16:07:28 +0100

    add buildable lib with scss mixin

--- apps/shell/rsbuild.config.ts
+++ apps/shell/rsbuild.config.ts
@@ -1,7 +1,13 @@
 import { createConfig } from '@ng-rsbuild/plugin-angular';
+import { pluginSass } from '@rsbuild/plugin-sass';
 
-export default createConfig({
-  browser: './src/main.ts',
-  inlineStylesExtension: 'scss',
-  styles: ['./src/styles.scss'],
-});
+export default createConfig(
+  {
+    browser: './src/main.ts',
+    inlineStylesExtension: 'scss',
+    styles: ['./src/styles.scss', './src/vendors.scss'],
+  },
+  {
+    plugins: [pluginSass()],
+  }
+);
---
+++ libs/mylib/src/lib/mylib/mylib.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-mylib',
+  imports: [CommonModule],
+  templateUrl: './mylib.component.html',
+  styleUrl: './mylib.component.scss',
+})
+export class MylibComponent {}
